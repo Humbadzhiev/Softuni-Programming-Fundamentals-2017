75526ac67b64a5e0ac83ad26930bf071708afc6a
Hunbadzhiev <humbi@mail.bg>
2017-03-28 11:55:31 +0300

    Second test

---
+++ Advanced Collections Exercises/1. Shellbound/Shellbound.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Shellbound
+{
+    class Shellbound
+    {
+        static void Main(string[] args)
+        {
+            var regionName = new Dictionary<string, List<int>>();
+
+            var input = Console.ReadLine();
+
+            while (input != "Aggregate")
+            {
+                var tokens = input.Split(' ');
+                var region = tokens[0];
+                var shells = int.Parse(tokens[1]);
+
+                if (!regionName.ContainsKey(region))
+                {
+                    regionName[region] = new List<int>();
+
+                }
+
+                if (!regionName[region].Contains(shells))
+                {
+                    regionName[region].Add(shells);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var pair in regionName)
+            {
+                var regions = pair.Key;
+                var vladiShells = pair.Value;
+                var average = vladiShells.Average();
+                var sumShells = vladiShells.Sum();
+                var pcsShell = vladiShells.Count();
+                var giantShell = sumShells - sumShells / pcsShell;
+
+                Console.Write($"{regions} -> ");
+
+                Console.Write(string.Join(", ", vladiShells));
+
+                Console.WriteLine($" ({giantShell})");
+            }
+        }
+    }
+}
---
+++ Advanced Collections Exercises/2. Dict-Ref-Advanced/DictRefAdvanced.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Dict_Ref_Advanced
+{
+    class DictRefAdvanced
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+
+            var resultDict = new Dictionary<string, int>();
+
+            while (line != "end")
+            {
+                var tokens = line.Split();
+
+                var firstElement = tokens[0];
+                var lastElement = tokens[tokens.Length - 1];
+
+                var number = 0;
+
+                if (int.TryParse(lastElement, out number))
+                {
+                    resultDict[firstElement] = number;
+                }
+                else
+                {
+                    if (resultDict.ContainsKey(lastElement))
+                    {
+                        var refValue = resultDict[lastElement];
+                        resultDict[firstElement] = refValue;
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+            foreach (var itemPricePair in resultDict)
+            {
+                var item = itemPricePair.Key;
+                var price = itemPricePair.Value;
+
+                Console.WriteLine($"{item} === {price}");
+            }
+        }
+    }
+}
